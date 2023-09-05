356035ce2511e6d24924885284dbf71065922997
RostHarcha <rostiki.com@mail.ru>
2023-09-05 19:07:23 +0300

    handling pop-up errors

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -5,35 +5,38 @@ class Parser:
     def __init__(self) -> None:
         self.driver = driver.get_driver()
 
-    def wait(self, method):
+    def wait_element(self, by: str, value: str) -> driver.WebElement:
         time.sleep(5)
         return driver.WebDriverWait(
-            self.driver,
-            20,
-            2
+            driver=self.driver,
+            timeout=6,
+            poll_frequency=2
         ).until(
-            method
+            method=driver.EC.presence_of_element_located((by, value))
         )
-
-    def wait_element(self, by: str, value: str) -> driver.WebElement:
-        return self.wait(driver.EC.presence_of_element_located((by, value)))
+        
+    def check_no_errors(self) -> bool:
+        errors = self.driver.find_elements(driver.By.CLASS_NAME, 'rros-ui-lib-error-title')
+        if errors == []:
+            return True
+        for error in errors:
+            error.find_element(driver.By.TAG_NAME, 'button').click()
+        return False
     
     def click_element(self, by: str, value: str):
-        for i in range(5):
+        for _ in range(5):
+            self.wait_element(by, value).click()
             time.sleep(1)
-            try:
-                self.wait_element(by, value).click()
+            if self.check_no_errors():
                 return
-            except:
-                pass
+            time.sleep(2)
+        raise
 
     def login(self):
         self.driver.get('http://lk.rosreestr.ru/request-access-egrn/property-search')
-        try:
+        if self.driver.find_elements(driver.By.ID, 'details-button') != []:
             self.driver.find_element(driver.By.ID, 'details-button').click()
             self.driver.find_element(driver.By.ID, 'proceed-link').click()
-        except:
-            pass
 
     def request_EGRN(self, value: str):
         self.driver.get('https://lk.rosreestr.ru/request-access-egrn/')
@@ -42,3 +45,4 @@ class Parser:
         self.click_element(driver.By.CLASS_NAME, 'rros-ui-lib-table__row')
         self.click_element(driver.By.CLASS_NAME, 'build-card-wrapper__header-wrapper__action-button')
         self.click_element(driver.By.CLASS_NAME, 'rros-ui-lib-link_inherit')
+        return self.wait_element(driver.By.CLASS_NAME, 'success-view').text == 'Ваша заявка отправлена в ведомство'
